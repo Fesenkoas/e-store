9a1799b78814894ef39edc8bf9ed0a85427b0949
Aleksandr Fesenko <fesenkoas@gmail.com>
2022-10-20 18:13:39 +0300

    Update Categories.jsx

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 import { categoriesItem } from '../unit/constant'
 
 const Categories = ({chooseCategory}) => {
-    const [categories, setCategories] =useState(categoriesItem)
+    //const [categories, setCategories] =useState(categoriesItem)
   return (
     <div className='categories'>
-{categories.map(el => (
+{categoriesItem.map(el => (
     <div key={el.id} onClick={()=>chooseCategory(el.key)}>{el.name}</div>
 ))}
     </div>
